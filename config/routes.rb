5d0bbbed343b3e636dbc6388769409f0d1971448
Zac Stewart <zgstewart@gmail.com>
2012-09-24 02:48:05 -0400

    Add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ RetrospectionApp::Application.routes.draw do
   devise_for :users
 
   resources :sleeps, except: :show
-  resources :retrospections, except: :new, path: '/'
-  root to: 'retrospections#index'
+  resources :retrospections, except: :new
+  root to: 'application#home'
 end
