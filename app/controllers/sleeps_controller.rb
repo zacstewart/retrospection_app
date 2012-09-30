f38e3668b791f26feefed491cd0ee0ee0af47659
Zac Stewart <zgstewart@gmail.com>
2012-09-30 14:41:00 -0400

    Trim whitespace

--- app/controllers/sleeps_controller.rb
+++ app/controllers/sleeps_controller.rb
@@ -1,6 +1,6 @@
 class SleepsController < ApplicationController
   before_filter :authenticate_user!
-  
+
   def index
     @sleeps = SleepDecorator.decorate(current_user.sleeps.chronological)
   end
@@ -12,10 +12,10 @@ class SleepsController < ApplicationController
   def edit
     @sleep = current_user.sleeps.find(params[:id])
   end
-  
+
   def create
     @sleep = current_user.sleeps.new(params[:sleep])
-    
+
     if @sleep.save
       flash[:success] = 'Successfully logged sleep'
       redirect_to Sleep
