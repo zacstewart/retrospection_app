d7d7e1fb5ef6b5d4133f6c7b98a70401e3289e02
Zac Stewart <zgstewart@gmail.com>
2012-09-30 02:55:14 -0400

    Trim whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
   has_many :retrospections
   has_many :sleeps
 
